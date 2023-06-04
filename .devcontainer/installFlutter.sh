305d78072fe566f5ecef908ceb3e32126e7d7778
Wil Marques <wileymarques@live.com>
2023-06-04 13:29:27 +0000

    WIP

--- .devcontainer/installFlutter.sh
+++ .devcontainer/installFlutter.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-# Install Flutter dependencies
-sudo apt update
-sudo apt install -y curl git unzip xz-utils zip libglu1-mesa
+# Install Flutter and its dependencies
+# Ready for Android, Web and Linux desktop
 
-# Install Flutter for Linux desktop dependencies
-sudo apt install -y clang cmake ninja-build pkg-config libgtk-3-dev
-
-# Go to home dir
+# Go to $HOME
 cd $HOME
 
+# Update apt package
+sudo apt update
+
+# Install Flutter dependencies not available in the base image (Ubuntu)
+sudo apt install -y xz-utils libglu1-mesa
+
+# Install Flutter for Linux desktop dependencies not available in the base image (Ubuntu)
+sudo apt install -y ninja-build libgtk-3-dev
+
+# Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome.deb
+sudo apt install -y ./google-chrome.deb
+rm -rf google-chrome.deb
+
 # Download Flutter 3.10.3 for Linux naming the file as flutter.tar.xz
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.10.3-stable.tar.xz -O flutter.tar.xz
-# Extract flutter.tar.xz to $HOME
+wget "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_$FLUTTER_VERSION-stable.tar.xz" -O flutter.tar.xz
+# Extract flutter.tar.xz
 tar -xf flutter.tar.xz
 # Rename flutter dir to .flutter
 mv flutter .flutter
-# Add flutter to PATH
+# Add flutter bin to PATH
 echo 'export PATH="$PATH:$HOME/.flutter/bin"' >> ~/.bashrc
+
+# Return to previous dir
+cd -
